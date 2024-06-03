c5fa4d045b99b2a793dbd44a36b2fed75e399228
olewna <dajcie.suba2002@gmail.com>
2024-06-03 11:22:50 +0200

    added logic for compiler component

--- client/src/app/compiler/compiler.component.ts
+++ client/src/app/compiler/compiler.component.ts
@@ -1,15 +1,42 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+declare global {
+  interface Window {
+    SEC?: any;
+  }
+}
 
 @Component({
   selector: 'app-compiler',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './compiler.component.html',
   styleUrl: './compiler.component.scss',
 })
 export class CompilerComponent {
   constructor(private readonly location: Location) {}
+
+  public id: string = '';
+  public idInput: string = '';
+  public loading: boolean = false;
+  private widget: any;
+
+  public setId(): void {
+    this.id = this.idInput;
+    this.idInput = '';
+    this.loading = true;
+    setTimeout(() => {
+      this.widget = window.SEC.widget('example-widget');
+      this.loading = false;
+    }, 1000);
+  }
+
+  public deleteId(): void {
+    this.id = '';
+    this.widget.destroy();
+  }
 
   public goBack(): void {
     this.location.back();
