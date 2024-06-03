21d96296c5baad7f2b4d4b7dd770c97855de255d
olewna <dajcie.suba2002@gmail.com>
2024-06-03 12:26:30 +0200

    added tests for compiler get and post

--- server/src/compiler/compiler.controller.spec.ts
+++ server/src/compiler/compiler.controller.spec.ts
@@ -1,20 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompilerController } from './compiler.controller';
 import { CompilerService } from './compiler.service';
+import {
+  CreateCompilerDto,
+  CreateCompilerResponseDto,
+  GetCompilerDto,
+} from './dto/compiler.dto';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 describe('CompilerController', () => {
   let controller: CompilerController;
+  let service: CompilerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [CompilerController],
       providers: [CompilerService],
     }).compile();
 
     controller = module.get<CompilerController>(CompilerController);
+    service = module.get<CompilerService>(CompilerService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('createSubmission => should create new compiler and return its ID', async () => {
+    const createCompilerDto: CreateCompilerDto = {
+      compilerId: 12,
+      source: "console.log('test')",
+    };
+
+    const response: CreateCompilerResponseDto = {
+      id: '2184yhf9i9uuh38291g7dh8f9202',
+    };
+
+    jest
+      .spyOn(service, 'createSubmission')
+      .mockImplementation(() => new Promise((resolve) => resolve(response)));
+
+    const result = await service.createSubmission(createCompilerDto);
+
+    expect(service.createSubmission).toHaveBeenCalled();
+    expect(service.createSubmission).toHaveBeenCalledWith(createCompilerDto);
+
+    expect(result).toEqual(response);
+  });
+
+  it('getSubmission => should return submission by ID', async () => {
+    const submissionId = '2184yhf9i9uuh38291g7dh8f9202';
+    const submission: GetCompilerDto = {
+      id: 1,
+      executing: false,
+      date: '02.12.2022',
+      compiler: null,
+      result: null,
+    };
+
+    jest
+      .spyOn(service, 'getSubmission')
+      .mockImplementation(() => new Promise((resolve) => resolve(submission)));
+
+    const result = await controller.getSubmission(submissionId);
+
+    expect(service.getSubmission).toHaveBeenCalledWith(submissionId);
+    expect(result).toEqual(submission);
   });
 });
--- server/src/compiler/compiler.service.spec.ts
+++ server/src/compiler/compiler.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompilerService } from './compiler.service';
+import {
+  CreateCompilerDto,
+  CreateCompilerResponseDto,
+} from './dto/compiler.dto';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { throwError } from 'rxjs';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CompilerService', () => {
   let service: CompilerService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [CompilerService],
     }).compile();
 
     service = module.get<CompilerService>(CompilerService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('createSubmission => should return promise', async () => {
+    const createCompilerDto: CreateCompilerDto = {
+      compilerId: 12,
+      source: "console.log('test')",
+    };
+
+    const response: CreateCompilerResponseDto = {
+      id: '2184yhf9i9uuh38291g7dh8f9202',
+    };
+
+    jest
+      .spyOn(service, 'createSubmission')
+      .mockImplementation(() => new Promise((resolve) => resolve(response)));
+
+    const result = await service.createSubmission(createCompilerDto);
+
+    expect(service.createSubmission).toHaveBeenCalled();
+    expect(service.createSubmission).toHaveBeenCalledWith(createCompilerDto);
+
+    expect(result).toEqual(response);
+  });
+
+  it('createSubmission => should throw NotFoundException', async () => {
+    const createCompilerDto: CreateCompilerDto = {
+      compilerId: 1,
+      source: "console.log('test')",
+    };
+
+    jest
+      .spyOn(httpService, 'post')
+      .mockReturnValueOnce(throwError(() => new Error('Error')));
+
+    await expect(service.createSubmission(createCompilerDto)).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
+  it('getSubmission => should throw NotFoundException', async () => {
+    const id: string = '1';
+
+    jest
+      .spyOn(httpService, 'get')
+      .mockReturnValueOnce(throwError(() => new Error('Error')));
+
+    await expect(service.getSubmission(id)).rejects.toThrow(NotFoundException);
   });
 });
